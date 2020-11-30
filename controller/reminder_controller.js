4f7834930b52d5efd08e52f80d79eee0b994cb92
Oliver <62718139+2571792460@users.noreply.github.com>
2020-11-30 14:57:00 -0800

    123

--- controller/reminder_controller.js
+++ controller/reminder_controller.js
@@ -1,17 +1,14 @@
 let database = require("../database");
 
 let remindersController = {
-  // Show a list of reminders
   list: (req, res) => {
     res.render('reminder/index', { reminders: database.cindy.reminders })
   },
 
-  // Show a Create Reminder Page
   new: (req, res) => {
     res.render('reminder/create')
   },
 
-  // Show the details of a Single Reminder
   listOne: (req, res) => {
     let reminderToFind = req.params.id;
     let searchResult = database.cindy.reminders.find(function (reminder) {
@@ -24,8 +21,6 @@ let remindersController = {
     }
   },
 
-  // Create a reminder
-  // ⚠️ TODO: Currently hardcoded to always create a reminder for Cindy only. You need to make this dynamic. 
   create: (req, res) => {
     let reminder = {
       id: database.cindy.reminders.length + 1,
@@ -38,38 +33,33 @@ let remindersController = {
   },
 
   edit: (req, res) => {
-    // ⭐️ your implementation here ⭐️
     let reminderToFind = req.params.id;
     let searchResult = database.cindy.reminders.find(function (reminder) {
       return reminder.id == reminderToFind;
     })
     res.render('reminder/edit', { reminderItem: searchResult })
 
-
   },
 
- // Edit the Reminder
- update: (req, res) => {
-  // ⭐️ your implementation here ⭐️
-  let reminderToFind = req.params.id;
-  let searchResult = database.cindy.reminders.find(function(reminder){
-    if (reminder.id == reminderToFind) {
-      reminder.title = req.body.title,
-      reminder.description = req.body.description,
-      reminder.completed = req.body.completed == "true"
-    }
-  });
-  res.redirect('/reminder/'+reminderToFind)
-},
+  update: (req, res) => {
+    let reminderToFind = req.params.id;
+    let searchResult = database.cindy.reminders.find(function (reminder) {
+      if (reminder.id == reminderToFind) {
+        reminder.title = req.body.title,
+          reminder.description = req.body.description,
+          reminder.completed = req.body.completed == "true"
+      }
+    });
+    res.redirect('/reminder/' + reminderToFind)
+  },
 
-  // Delete the Reminder
   delete: (req, res) => {
-    // ⭐️ your implementation here ⭐️
-    let reminderToDelete = req.params.id;
-    let obj = database.cindy.reminders.find(obj => obj.id == reminderToDelete)
-    let deleteIndex = database.cindy.reminders.indexOf(obj);
-    database.cindy.reminders.splice(deleteIndex, 1);
-    res.redirect('/reminders')
+    let reminderToFind = req.params.id;
+    let reminderIndex = database.cindy.reminders.findIndex(function (reminder) {
+      return reminder.id == reminderToFind;
+    })
+    database.cindy.reminders.splice(reminderIndex, 1);
+    res.redirect('/reminders');
   }
 }
 
